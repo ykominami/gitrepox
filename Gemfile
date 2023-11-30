ec9897d297f7c93129703a0395717a6836d52084
ykominami <ykominami@gmail.com>
2023-11-30 20:43:56 +0900

    correct by rubocop

--- Gemfile
+++ Gemfile
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in gitx.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem 'rake', '~> 13.0'
 
-gem "rspec", "~> 3.0"
+gem 'rspec', '~> 3.0'
 
-gem "git"
+gem 'git'
 gem 'google_drive'
+
+# gem "secretmgr", path: "../secretmgr"
+gem 'secretmgr', '0.1.0'
--- lib/gitx/gsession.rb
+++ lib/gitx/gsession.rb
@@ -1,14 +1,40 @@
-require "git"
+require 'git'
+require 'secretmgr'
+require 'pathname'
 
 module Gitx
   class Gsession
-    def initialize(config = "config.json")
+    def initialize(config = 'config.json')
+      ensure_config_file(config)
       @session = GoogleDrive::Session.from_config(config)
     end
-    
-    def upload_from_string(content, filename, content_type = "text/tab-separated-values")
-      obj = @session.upload_from_string(content, filename, content_type: content_type ) 
-      [obj.title, obj.id].join(",")
+
+    def ensure_config_file(fname)
+      pn = Pathname.new(fname)
+      return if pn.exist?
+
+      target = 'Google'
+      sub_target = 'GCPX'
+      make_config_file(config, target, sub_target)
+    end
+
+    def make_config_file(fname, target, sub_target)
+      home_dir = ENV['HOME']
+      home_pn = Pathname.new(home_dir)
+      secret_dir_pn = "#{home_pn}secret"
+      sm = Secretmgr::Secretmgr.new(secret_dir_pn)
+      sm.set_setting_for_query(target, sub_target)
+      content = sm.load
+      File.write(fname, content)
+    end
+
+    def delete_config_file(fname)
+      File.delete(fname)
+    end
+
+    def upload_from_string(content, filename, content_type = 'text/tab-separated-values')
+      obj = @session.upload_from_string(content, filename, content_type: content_type)
+      [obj.title, obj.id].join(',')
     end
   end
 end
